611dcaa960ad9c0a468c918ec39a648999362ec9
Saad Lakhani <muhammad.saad.002002@gmail.com>
2023-12-05 20:01:07 +0500

    Improve frontend

--- frontend/src/pages/AlbumPage.js
+++ frontend/src/pages/AlbumPage.js
@@ -1,57 +1,98 @@
 import React from 'react';
-import albumCover from '../logo.svg'; // Replace with your album cover image
+import '../styles/AlbumPage.css';
 
-// AlbumHeader component
-const AlbumHeader = () => (
-  <div style={{ display: 'flex', alignItems: 'center', padding: '20px', color: 'white' }}>
-    <img src={albumCover} alt="Album Cover" style={{ width: '150px', height: '150px', borderRadius: '10px' }} />
-    <div style={{ marginLeft: '20px' }}>
-      <h1>1989 (Taylor's Version)</h1>
-      <p>Taylor Swift - 21 Songs, 1 hr 17 mins</p>
+const AlbumPage = () =>{
+  return (
+    <div className='main-container'>
+      <div className='rectangle'>
+        <div className='auto-layer-row-bbf'>
+          <div className='abdbfefeacbe'>
+            <span className='abdullah-shahood'>Abdullah Shahood</span>
+          </div>
+          <div className='screenshot-at' />
+          <span className='album'>Album</span>
+          <span className='text-3'>1989 (Taylor’s Version)</span>
+          <div className='screenshot-at-1' />
+          <span className='taylor-swift-songs'>
+            Taylor Swift · 21 Songs, 1 hr 17 mins 
+          </span>
+        </div>
+        <div className='auto-layer-row-eae'>
+          <div className='untitled-design' />
+          <div className='rectangle-2'>
+            <span className='search-bar'>Search Bar </span>
+            <div className='magnifying-glass' />
+          </div>
+          <div className='clock' />
+          <span className='title'>Title </span>
+          <span className='album-3'>
+            Album
+            <br />
+          </span>
+          <div className='line' />
+        </div>
+        <span className='span-4'>4..</span>
+        <span className='span-4-4'>4.</span>
+        <div className='rectangle-5' />
+        <div className='rectangle-6' />
+        <div className='auto-layer-row-ce'>
+          <div className='line-7' />
+          <div className='screenshot-at-8' />
+          <span className='span-3'>3.</span>
+          <span className='all-you-had-to-do-was-stay'>
+            All You Had To Do Was Stay (Taylor’s Version)
+            <br />
+            <br />
+            Taylor Swift
+          </span>
+          <span className='text-c'>
+            1989 (Taylor’s Version)
+            <br />
+          </span>
+          <span className='time'>
+            3:13
+            <br />
+          </span>
+        </div>
+        <div className='auto-layer-row-cfa'>
+          <div className='screenshot-at-9' />
+          <span className='welcome-to-new-york'>
+            Welcome To New York (Taylor’s Version)
+            <br />
+            <br />
+            Taylor Swift
+          </span>
+          <span className='text-f'>
+            1989 (Taylor’s Version)
+            <br />
+          </span>
+          <span className='time-a'>
+            3:32
+            <br />
+          </span>
+          <span className='dot'>1.</span>
+        </div>
+        <div className='auto-layer-row-b'>
+          <div className='screenshot-at-b' />
+          <span className='blank-space-taylors-version'>
+            Blank Space (Taylor’s Version)
+            <br />
+            <br />
+            Taylor Swift
+          </span>
+          <span className='text-13'>
+            1989 (Taylor’s Version)
+            <br />
+          </span>
+          <span className='colon'>
+            3:51
+            <br />
+          </span>
+          <span className='dot-c'>2.</span>
+        </div>
+      </div>
     </div>
-  </div>
-);
-
-// SearchBar component
-const SearchBar = () => (
-  <div style={{ padding: '0 20px' }}>
-    <input
-      type="text"
-      placeholder="Search Bar"
-      style={{
-        width: '100%',
-        padding: '10px',
-        margin: '10px 0',
-        borderRadius: '5px',
-        border: 'none'
-      }}
-    />
-  </div>
-);
-
-// SongRow component
-const SongRow = ({ number, title, album, duration }) => (
-  <div style={{ display: 'flex', alignItems: 'center', color: 'white', padding: '10px 20px' }}>
-    <div style={{ width: '50px', marginRight: '20px' }}>{number}.</div>
-    <img src={albumCover} alt="Artist" style={{ width: '50px', height: '50px', borderRadius: '50%' }} />
-    <div style={{ marginLeft: '20px', flexGrow: 1 }}>
-      <div style={{ fontWeight: 'bold' }}>{title}</div>
-      <div>{album}</div>
-    </div>
-    <div style={{ marginLeft: '20px' }}>{duration}</div>
-  </div>
-);
-
-// Main Album Page component
-const AlbumPage = () => (
-  <div style={{ backgroundColor: '#1E2A47', height: '100vh', overflow: 'auto' }}>
-    <AlbumHeader />
-    <SearchBar />
-    <div>
-      <SongRow number={1} title="Welcome To New York (Taylor's Version)" album="1989 (Taylor's Version)" duration="3:32" />
-      {/* Repeat SongRow for each song */}
-    </div>
-  </div>
-);
+  );
+}
 
 export default AlbumPage;
