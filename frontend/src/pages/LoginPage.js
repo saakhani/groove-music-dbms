80b610d4e0e79d77e341b601da7f1bad512910c6
TK-474 <talalkhan213@gmail.com>
2023-12-10 17:41:46 +0500

    updated login, signup, home pages

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import '../styles/LoginPage.css';
+import '../styles/LoginPage.css'; // Import the CSS file
 
-const LoginPage = () => {
+const Login = () => {
   return (
-    <div className='main-container'>
-      <div className='rectangle'>
-        <div className='auto-layer-row-b'>
-          <div className='draft' />
-          <span className='login'>Login</span>
-        </div>
-        <span className='signup'>Signup</span>
-        <span className='reset-password'>Reset Password</span>
-        <div className='rectangle-1' />
-        <div className='rectangle-2' />
+    <div>
+      <img className="logo" src="DRAFT.png" alt="Logo" />
+      <div className="login-box">
+        <h2>Login</h2>
+        <form action="homepage.html" method="post">
+          <input type="text" placeholder="Username" required />
+          <input type="password" placeholder="Password" required />
+          <button type="submit">Log In</button>
+        </form>
+        <a href="signup.html" className="signup-link">Sign Up</a>
       </div>
     </div>
   );
-}
+};
 
-export default LoginPage;
+export default Login;
--- frontend/src/pages/SignupPage.js
+++ frontend/src/pages/SignupPage.js
@@ -1,59 +1,26 @@
 import React from 'react';
-import '../styles/SignupPage.css';
+import '../styles/SignupPage.css'; // Import the CSS file
 
-const Sidebar = () => {
-  return (
-    <div className='main-container'>
-      <div className='rectangle'>
-        <div className='draft' />
-        <div className='frame-1'>
-          <div className='sign-up-info'>
-            <div className='rectangle-2' />
-            <div className='auto-layer-row'>
-              <div className='group'>
-                <div className='auto-layer-row-de'>
-                  <span className='username'>Username</span>
-                  <div className='user-outline' />
-                </div>
-                <div className='auto-layer-row-3'>
-                  <div className='rectangle-4' />
-                  <span className='first-name'>First Name</span>
-                  <div className='user-outline-5' />
-                </div>
-                <div className='auto-layer-row-b'>
-                  <div className='rectangle-6' />
-                  <span className='last-name'>Last Name</span>
-                  <div className='user-outline-7' />
-                </div>
-                <div className='user-outline-8' />
-                <div className='user-outline-9' />
-                <div className='auto-layer-row-d'>
-                  <div className='lock' />
-                  <span className='password'>Password</span>
-                </div>
-                <div className='auto-layer-row-e'>
-                  <div className='rectangle-a' />
-                  <span className='re-enter-password'>Re-enter Password</span>
-                  <div className='lock-b' />
-                </div>
-                <div className='auto-layer-row-c'>
-                  <span className='signup'>Signup</span>
-                  <div className='rectangle-d' />
-                </div>
-              </div>
-              <div className='rectangle-e' />
-              <span className='email'>Email</span>
+const Signup = () => {
+    return (
+        <div>
+            <img className="logo" src="DRAFT.png" alt="Logo" />
+            <div className="registration-box">
+                <h2>Sign Up</h2>
+                <form>
+                    <input type="text" placeholder="Username" required />
+                    <input type="text" placeholder="First Name" required />
+                    <input type="text" placeholder="Last Name" required />
+                    <input type="email" placeholder="Email" required />
+                    <input type="date" placeholder="Date of Birth" required />
+                    <input type="password" placeholder="Password" required />
+                    <input type="password" placeholder="Re-enter Password" required />
+                    <button type="submit">Sign Up</button>
+                </form>
+                <a href="login.html" className="signup-link">Already have an account? Login</a>
             </div>
-            <div className='auto-layer-row-db'>
-              <span className='date-of-birth'>Date of Birth</span>
-              <div className='rectangle-f' />
-            </div>
-            <div className='rectangle-10' />
-          </div>
         </div>
-      </div>
-    </div>
-  );
-}
+    );
+};
 
-export default Sidebar;
+export default Signup;
